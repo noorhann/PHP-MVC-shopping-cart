fee4f3d4c54b06c71905301850e85256583cd447
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2021-08-23 19:37:50 +0200

    Add files via upload

---
+++ app/bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+require_once 'config/config.php';
+require_once 'libraries/controller.php';
+require_once 'libraries/core.php';
+require_once 'libraries/database.php';
+require_once 'helpers/helpers.php';
+require_once 'helpers/session.php';
+
+?>    
