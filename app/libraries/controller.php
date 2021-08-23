a79c65761c2ee7ea5bad80aa74a8c1e4de272b3b
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2021-08-23 19:42:25 +0200

    Add files via upload

---
+++ app/libraries/controller.php
@@ -0,0 +1,19 @@
+<?php
+class controller{
+
+    public function model($model){
+        require_once APPROOT.'/models/'. $model . '.php';
+        
+        return new $model();
+    }
+    public function view($view, $data=[] ){
+        if (file_exists(APPROOT.'/views/'. $view .'.php')){
+            require_once APPROOT.'/views/'. $view .'.php';
+
+
+        }else{
+            die('view does not exit');
+        }
+    }
+}
+?>
