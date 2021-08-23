bc91f9984e657c65e86cc9d1b8f69952b23e6f81
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2021-08-23 19:39:07 +0200

    Add files via upload

---
+++ app/config/config.php
@@ -0,0 +1,13 @@
+<?php
+
+define('APPROOT',dirname(dirname(__FILE__))) ;
+define('URLROOT','http://localhost/shoppingCart/') ;
+define('SITENAME','shoppingCart');
+
+define('DB_HOST' , 'localhost');
+define('DB_USER' , 'root');
+define('DB_PASS' , '');
+define('DB_NAME' , 'onlinestore');
+
+
+?>
