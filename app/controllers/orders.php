6a12f8bbfb1a9a28ef6250d30d00cb7d952a6e5c
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2021-08-28 07:55:21 +0200

    Update orders.php

--- app/controllers/orders.php
+++ app/controllers/orders.php
@@ -11,34 +11,38 @@ class orders extends controller
 
     }
 
-    public function checkout($order_id)
+    public function checkout()
     {
-        $_SESSION['order_id']=$order_id;
-        $cart = $this->cartModel->getForOrder($order_id);
+        $cart = $this->cartModel->getForOrder();
         $this->view('orders/checkout' , $cart);
     }
 
     public function order()
     {
-        $cart = $this->cartModel->getForOrder($_SESSION['order_id']);
+
+        $cart = $this->cartModel->getForOrder($_SESSION['user_id']);
         $orders = $this->detailsModel;
         foreach($cart as $cart):
-            $orders->order_id = $_SESSION['order_id'];
+            $orders->order_id = $this->cartModel->get_order_id($_SESSION['user_id'])->order_id;
             $orders->qty=$cart->qty;
-            $orders->item_name=$cart->item_name;
-            $orders->price=$cart->price;
             $orders->product_id=$cart->product_id;
+            $orders->item_name=$cart->product_name;
+            $orders->price=$cart->product_price;
             $orders->create();
         endforeach ;
-        //$this->cartModel->clear($_SESSION['order_id']);
+
         $order =$this->orderModel;
-        $order->order_id = $_SESSION['order_id'];
+        $order->order_id = $this->cartModel->get_order_id($_SESSION['user_id'])->order_id;
         $order->user_id = $_SESSION['user_id'];
-        $order->Address='shoubra';
-        $order->Phone_no=0123456;
-        $order->Full_Name=$_SESSION['user_name'];
+        $order->Address = 'Shoubra';
+        $order->Phone_no="0123456789";
+        $order->Full_Name = $_SESSION['user_id'];
         $order->create();
+    
+        $this->cartModel->deleteTemp($_SESSION['user_id']);
+        $this->cartModel->clear();
         redirect('');
+
     }
 
 
