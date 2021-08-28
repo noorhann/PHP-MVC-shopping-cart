ae771c4445caee427178cb7ec58e097c67ef02db
Norhan Taher <48496858+noorhann@users.noreply.github.com>
2021-08-28 07:54:44 +0200

    Update cart.php

--- app/controllers/cart.php
+++ app/controllers/cart.php
@@ -12,34 +12,40 @@ class cart extends controller
 
     public function add($id)
     {
-        $cart=$this->orderModel->findUserCart($_SESSION['user_id']);
-        if ($cart == null)
+        $cart=$this->cartModel->findUserCart($_SESSION['user_id']);
+        if ($cart == NULL )
         {
-            $cart=$this->orderModel;
+            $cart=$this->cartModel;
             $cart->user_id = $_SESSION['user_id'];
-            $cart->order_id = $cart->temp();
+            $cart->temp();
         }
         $newitem = $this->cartModel;
         $newitem->product_id=$id;
         $newitem->qty =1;
-        $newitem->order_id=$cart->order_id;
-        $newitem->item_name =$this->productModel->find($id)->product_name ;
-        $newitem->price=$this->productModel->find($id)->product_price;
+        $newitem->user_id = $_SESSION['user_id'];
         $newitem ->create();
         redirect('');
     }
 
     public function view_cart()
     {
-        $cart=$this->orderModel->findUserCart($_SESSION['user_id']);
-        $items= $this->cartModel->getForOrder($cart->order_id);
+        $cart=$this->cartModel->findUserCart($_SESSION['user_id']);
+        $items= $this->cartModel->getForOrder();
         $this->view('cart/view_cart' , $items);
     }
 
-    public function removeFromCart($order_details_id)
+    public function removeFromCart($product_id)
     {
-        $this->cartModel->delete($order_details_id);
+        $this->cartModel->delete($product_id);
         redirect('cart/view_cart');
+    }
+
+    
+    public function findUserCart($user_id)
+    {
+        $this->db->query("SELECT * from orders WHERE user_id=:user_id");
+        $this->db->bind(":user_id",$user_id);
+        return $this->db->single();
     }
     
 
